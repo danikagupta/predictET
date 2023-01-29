a8b909903421a69c1c57b37e4c03bd33a02d4eaf
Gupta <danika@gprof.com>
2023-01-29 14:54:03 -0800

    Update launch_app.py

--- scripts/launch_app.py
+++ scripts/launch_app.py
@@ -1,1 +1,1 @@
-!streamlit run apps/app.py --server.port $CDSW_APP_PORT --server.address 127.0.0.1
+!/home/cdsw/.local/bin/streamlit run apps/app.py --server.port $CDSW_APP_PORT --server.address 127.0.0.1
